376d51599d1b917ad1956453013215845d188fda
mana <mn.ctgl.ws@gmail.com>
2020-02-10 07:16:30 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,27 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
     has_secure_password
     has_many :microposts
+    has_many :relationships #関係があります
+    has_many :followings, through: :relationships, source: :follow #関係を通して自分のfollowingがあります,relationshipstableのfollowmodelを参照
+    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id' #その反対の関係もあります
+    has_many :followers, through: :reverses_of_relationship, source: :user #逆にrelationshipを通じてfollowerもいます,reversesofrelationshipテーブルのusermodelを表してる
+    
+    #また、Rails の命名規則により、User から Relationship を取得するとき、user_id が使用されます。
+    #そのため、逆方向では、foreign_key: 'follow_id' と指定して、 user_id 側ではないことを明示します。
+  
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship #if there is relationship can destroy
+  end
+
+  def following?(other_user) #フォローしているかの確認
+    self.followings.include?(other_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
- root to:'toppages#index'
- 
+  root to: 'toppages#index'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
- #見栄えを考慮してloginとlogoutと名付けている
- 
- get 'signup', to: 'users#new'
- resources :users, only: [:index, :show, :new, :create]
- 
- resources :microposts, only:  [:create, :destroy]
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  
+  end
+
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy] #relationship は中間テーブルなので index や show でユーザに見せるようなリソースではありません。
 end
